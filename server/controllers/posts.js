cbcd10dda63a4037663712b6739ad09a44413e8f
Nehal-P250 <pisarnehal@gmail.com>
2022-04-23 15:18:24 +0530

    1> post model + its C.R. contorllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,24 @@
+import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
-    res.send("post gets works");
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessage = await PostMessage.find();
+        console.log("getPost -> ", postMessage);
+        res.status(200).json(postMessage);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: error.message });
+    }
+}
+
+export const createPosts = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
