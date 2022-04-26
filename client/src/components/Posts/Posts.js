22c511e39c67150c70a6a7d685c43dedf4ef406b
Nehal-P250 <pisarnehal@gmail.com>
2022-04-26 16:15:11 +0530

    3> component + way to style material UI components using makeStyles & useStyles

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,18 @@
 import React from "react";
+import useStyles from "./styles";
 
-const Post = () => {
+import Post from "./Post/Post";
+
+const Posts = () => {
+    const classes = useStyles();
     return (
-        <h1>Post</h1>
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+
     )
 };
 
-export default Post;
+export default Posts;
