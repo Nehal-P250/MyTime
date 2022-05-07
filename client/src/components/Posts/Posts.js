5ba65f7595a800dde60cbcdef6a1c082b843c063
Nehal-P250 <pisarnehal@gmail.com>
2022-05-08 02:30:39 +0530

    9> Posts UI + created at date bug fix

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,8 @@
 import React from "react";
-import useStyles from "./styles";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
+import useStyles from "./styles";
 import Post from "./Post/Post";
 
 const Posts = () => {
@@ -9,11 +10,19 @@ const Posts = () => {
     const posts = useSelector((state) => state.posts)
     console.log("Fetching all posts from backend -> ", posts);
     return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid container className={classes.container} spacing={3} alignItems="stretch">
+                {
+                    posts.map(post => {
+                        return (
+                            <Grid item xs={12} sm={6}>
+                                <Post post={post} />
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>
+        )
 
     )
 };
